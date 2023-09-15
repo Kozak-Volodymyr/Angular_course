f8254db018407e0de916ed2460225be23a30bee1
Vozkoz <voloodia02@gmail.com>
2023-09-15 17:26:56 +0300

    Reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,7 @@
 import { Component,ViewChild,AfterViewInit } from '@angular/core';
 import { PostComponent } from './post/post.component';
-import { FormControl, NgForm, NgModel } from '@angular/forms';
-class Post{
-  id:number;
-  postTitle:string
-}
-interface User{
-  username?:string,
-  email?:string,
-  details?:string
-}
-class UserDetails implements User{
-  username?: string;
-  email?: string;
-  details?: string;
-  constructor(username,email,details){
-    this.username=username
-    this.details=details
-    this.email=email
-  }
-}
+import { FormArray, NgForm, NgModel, Validators } from '@angular/forms';
+import {FormGroup,FormControl,FormBuilder} from '@angular/forms'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -27,28 +9,65 @@ class UserDetails implements User{
 })
 
 export class AppComponent {
-title:string='Angular Course'
-count:number=285645;
-dcValue:number=3.8567
-price:number=13.234
-today:Date=new Date();
-postObj:object={
-  id:1,
-  postTitle:'Post1'
-}
-postArray:Array<string>=['post1','post2','post3'];
-userDetails={
-  name:'User 1',
-  city:'New York',
-  countryCode:'US'
-}
-text:string='asdfsgdhfgjklkjhfgdsadfghjkiSfdghtdjyuioypi[]poiuryetrewerwetyutiyoupiu'
-constructor(){}
-onSubmit(f:NgForm){
-  console.log(f.value)
-}
-getValue(f:NgModel){
-  console.log(f);
+form:any
+emailRegex:string='[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$'
+contactRegex:string='[380][0-9]{9}'
+constructor(fb:FormBuilder){
   
+  this.form=fb.group({
+    fullName:['',[Validators.required,Validators.minLength(5)]],
+    email:['',[Validators.required,Validators.pattern(this.emailRegex)]],
+    contactDetails:fb.group(
+      {
+        address:['',[Validators.required]],
+      shippingAddress:['',[Validators.required]],
+      contactNo:[['',Validators.required,
+        Validators.pattern(this.contactRegex)]],
+      }
+    ),
+    skills:fb.array([])
+  })
+/*   this.form=new FormGroup({
+    fullName:new FormControl('',),
+    email:new FormControl('',),
+    contactDetails:new FormGroup({
+      address:new FormControl('',[Validators.required]),
+      shippingAddress:new FormControl('',Validators.required),
+      contactNo:new FormControl('',[Validators.required,
+        Validators.pattern(this.contactRegex)])
+    }),
+    skills:new FormArray([])
+  }) */
 }
+get fullName(){
+  console.log(this.form.get('fullName'))
+  return this.form.get('fullName');
+}
+get Email(){
+  return this.form.get('email');
+}
+get Address(){
+  return this.form.get('contactDetails.address');
+}
+get shippingAddress(){
+  return this.form.get('contactDetails.shippingAddress');
+}
+get contactNo(){
+  return this.form.get('contactDetails.contactNo');
+}
+get Skills(){
+  return this.form.get('skills') as FormArray
+}
+onSubmit(){
+  console.log(this.form)
+}
+addSkills(skills:HTMLInputElement){
+  this.Skills.push(new FormControl(skills.value))
+  console.log(this.form.value);
+  skills.value='';
+}
+removeSkills(index){
+  this.Skills.removeAt(index);
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -24,7 +24,8 @@ import { PostService } from './services/post.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [PostService],
   bootstrap: [AppComponent]
